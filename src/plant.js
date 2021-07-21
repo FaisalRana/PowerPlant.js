981f6d6ad9f32d6e483e513cc46c7644cabc1977
Saoud <saoud@saoud.dev>
2021-07-20 19:08:39 -0700

    fixing security issues in package-lock.json and adding plant.js file
    Co-authored-by: Jesse Callahan <jessetylercallahan@gmail.com>

---
+++ src/plant.js
@@ -0,0 +1,19 @@
+class Plant {
+  constructor() {
+    this.water = 0;
+    this.soil = 0;
+    this.light = 0;
+  }
+
+  hydrate() {
+    this.water ++
+  }
+
+  feed() {
+    this.soil ++
+  }
+
+  giveLight() {
+    this.light ++
+  }
+}
